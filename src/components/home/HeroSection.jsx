1e57cd3103052b4f4c325e57a7a7f2549667e41d
Harsh Kumar gupta <hkg8558@gmail.com>
2025-12-13 23:46:09 +0530

    banner changed

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -5,10 +5,21 @@ import { motion } from 'framer-motion';
 import '../../assets/scss/pages/home.scss';
 
 const HeroSection = () => {
+  const [isMobile, setIsMobile] = React.useState(window.innerWidth <= 768);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <section className="farsight-fullbg-hero">
       <img
-        src="/assets/images/banner/hbanner.png"
+        src={isMobile ? "/assets/images/banner/habnner-mobile.png" : "/assets/images/banner/hbanner5.png"}
         alt="Farsight Hero Banner"
         className="hero-background-image"
       />
@@ -22,17 +33,17 @@ const HeroSection = () => {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1, ease: 'easeOut' }}
               >
-                <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-                  <Button as={Link} to="/contact-us" className=" btn-skew mb-md-3">
-                  <span className="btn-skew__text">Get Started</span>
-                    
-                  </Button>
-                </motion.div>
+                  <motion.div whileTap={{ scale: 0.95 }}>
+                    <Button as={Link} to="/contact-us" className="btn-skew mb-md-5">
+                      <span className="btn-skew__text">Get Started</span>
+                    </Button>
+                  </motion.div>
               </motion.div>
             </Col>
           </Row>
         </Container>
       </div>
+      
     </section>
   );
 };
